950899cc5782467536e9ffb5cd6755ec848c718a
anes89cok <anes89cok@gmail.com>
2015-05-05 10:57:38 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,35 @@
+<?php
+$adresa[0]="121 East McMillan Street, Cincinnati, OH 45219"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[0]="Cincinnati, OH 45219";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[0]="Mt Auburn";
+$telephone[0]=5137218888;// Broj koji se poziva na dugme CALL US
+
+$adresa[1]="8944 Columbia Road Loveland, OH 45140"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[1]="Cincinnati, OH 45410";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[1]="Landen";
+$telephone[1]=5137748800;// Broj koji se poziva na dugme CALL US
+
+$adresa[2]="2760 Mack Road Fairfield, OH 45014"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[2]="Cincinnati, OH 45014";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[2]="Fairfield";
+$telephone[2]=5138742444;// Broj koji se poziva na dugme CALL US
+
+$adresa[3]="4360 Ferguson Drive, Cincinnati, OH 45245"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[3]="Cincinnati, OH 45245";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[3]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[3]="Eastgate";
+$telephone[3]=5137536446;// Broj koji se poziva na dugme CALL US
+
+$adresa[4]="5976  Meijer Drive Suite 110B Milford, OH 45150"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[4]="Cincinnati, OH 45150";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[4]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[4]="Eastgate";
+$telephone[4]=5137121085;// Broj koji se poziva na dugme CALL US
+
+$companynumber=5;
+$theme="b";//Thema koja se aplicira na sve
+$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
